3e97a32065e4a4ef3cd103957a616a4f4ef13b52
Michael Rogers <Michael.Rogers@ukho.gov.uk>
2020-08-27 15:09:59 +0100

    Introduce the shape of the entry point as a console app

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import { program } from 'commander';
+
+program
+  .version('0.0.1')
+  .description('A CLI for generating markdown from JSON')
+  .option('-s, --source', 'Directory containing the JSON files, used for generating to .md files')
+  .option('-d, --destination', 'Directory to send the .md files to')
+  .parse(process.argv);
+
+console.log(`Description: ${program.description()}`);
