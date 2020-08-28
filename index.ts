b4a5889207d0cd89e5620739aa1dc2fc7cfec581
Michael Rogers <Michael.Rogers@ukho.gov.uk>
2020-08-28 19:46:38 +0100

    More help from CLI

--- index.ts
+++ index.ts
@@ -3,8 +3,20 @@
 import yargs = require('yargs');
 import { run } from './src/run';
 
-const argv = yargs.options({
-  src: { type: 'string', default: '' },
-}).argv;
+const argv = yargs
+  .options({
+    s: {
+      type: 'string',
+      default: '',
+      alias: 'source',
+      describe: 'The path to the source JSON file. Contents should obey the JSON Schema v3 or v4.'
+    }
+  })
+  .usage('Usage: $0 --source path')
+  .example(
+    '$0 --source C:\example.json',
+    'Returns formatted markdown based on the structure and properties of the example.json file.'
+  )
+  .argv;
 
-run(argv).then(console.log);
+run({src: argv.s}).then(console.log);
