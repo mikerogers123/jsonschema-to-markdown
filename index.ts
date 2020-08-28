9483546c5cdc45b163230006c676f4a900e4d2e8
Michael Rogers <Michael.Rogers@ukho.gov.uk>
2020-08-28 09:05:23 +0100

    Display help options on run

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
+
 import { program } from 'commander';
 
 program
   .version('0.0.1')
   .description('A CLI for generating markdown from JSON')
-  .option('-s, --source', 'Directory containing the JSON files, used for generating to .md files')
-  .option('-d, --destination', 'Directory to send the .md files to')
-  .parse(process.argv);
+  .option('-s, --source', 'Path to the JSON files, used for generating to markdown')
+  .option('-d, --destination', 'Directory to send the output markdown files to');
 
-console.log(`Description: ${program.description()}`);
+program.outputHelp();
